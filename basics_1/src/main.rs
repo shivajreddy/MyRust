f4d18e6cfc1fcd766b984f9ed97b529df5016147
ShivaSMBP <shivajayaramireddy@gmail.com>
2023-03-20 08:42:47 -0400

    .

---
+++ basics_1/src/main.rs
@@ -0,0 +1,14 @@
+
+fn main() {
+    println!("Hello, world!");
+
+
+    // create a vector of 5 characters
+
+    // iterate and print the 5 characters
+
+    // iterate and remove the characters from the vector
+
+    // iterate and add new characters
+
+}
