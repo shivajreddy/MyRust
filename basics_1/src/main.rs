84759442e7123a2d567621ea3c5a0fac52dd963c
ShivaSMBP <shivajayaramireddy@gmail.com>
2023-03-22 18:47:01 -0400

    rustlings 5 exercises

--- basics_1/src/main.rs
+++ basics_1/src/main.rs
@@ -1,14 +1,16 @@
-
 fn main() {
-    println!("Hello, world!");
-
     // create a vector of 5 characters
-    println!("hello 🦀 world");
 
     // iterate and print the 5 characters
 
     // iterate and remove the characters from the vector
 
     // iterate and add new characters
+    call_me(10);
+}
 
+fn call_me(num: i32) {
+    for i in 0..num {
+        println!("{i}");
+    }
 }
