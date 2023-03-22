ff3cf261d5e89660d097540ca71b3f52d4c937f0
ShivaSMBP <shivajayaramireddy@gmail.com>
2023-03-22 08:38:15 -0400

    rust basics

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -4,4 +4,28 @@ fn main() {
     println!("{}", 3 + 21);
 
     println!("{name} {fname}", fname = "shiva", name = "reddy");
+    println!("Base 10:{}", 21);
+    println!("Base 2:{:b}", 21);
+
+    let x: i32 = 21;
+    let y: i32 = 22;
+    println!("{:b}", x);
+    println!("{:b}", y);
+    println!("{:b}", y);
+
+    println!("{0}{1}{2}{3} ", 10, 20, 30, 40);
+    println!("My name is {0}, {1} {2}", "James", "Bond", "007");
+
+    let number: f64 = 1.21;
+    let width: usize = 5;
+
+    println!("number={number}, width={width}");
+    println!("{number:>width$}");
+    println!("end");
+    
+    let flag: bool = true;
+    println!("{flag}");
+    
+    println!("")
+
 }
